c643e502a37c929048a5bd3cc9b702a960fb50cf
MegumiKasuga <1926195023@qq.com>
2025-01-29 21:57:07 +0800

    - 科技树(3)

--- src/main/java/willow/train/kuayue/block/recipe/BlueprintScreen.java
+++ src/main/java/willow/train/kuayue/block/recipe/BlueprintScreen.java
@@ -11,12 +11,18 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
 import willow.train.kuayue.initial.ClientInit;
 import willow.train.kuayue.systems.tech_tree.client.ClientTechTree;
+import willow.train.kuayue.systems.tech_tree.client.ClientTechTreeGroup;
 import willow.train.kuayue.systems.tech_tree.client.ClientTechTreeManager;
+import willow.train.kuayue.systems.tech_tree.client.ClientTechTreeNode;
+import willow.train.kuayue.systems.tech_tree.client.gui.TechTreeItemButton;
+import willow.train.kuayue.systems.tech_tree.client.gui.TechTreeLabel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @OnlyIn(Dist.CLIENT)
 public class BlueprintScreen extends AbstractContainerScreen<BlueprintMenu> {
@@ -24,15 +30,43 @@ public class BlueprintScreen extends AbstractContainerScreen<BlueprintMenu> {
     private final HashMap<String, ClientTechTree> trees;
     LazyRecomputable<ImageMask> bgMask = LazyRecomputable.of(() -> new ImageMask(ClientInit.blueprintTableBg.getImageSafe().get()));
     LazyRecomputable<ImageMask> bgNoSubMask = LazyRecomputable.of(() -> new ImageMask(ClientInit.blueprintTableNoSub.getImageSafe().get()));
+    LazyRecomputable<ImageMask> upArrow = LazyRecomputable.of(
+            () -> ClientInit.blueprintButtons.getImageSafe().get().getMask()
+                    .copyWithOp(o -> o.rectangleUV(32f / 128f, 32f / 128f,
+                            48f / 128f, 40f / 128f))
+    );
+
+    LazyRecomputable<ImageMask> downArrow = LazyRecomputable.of(
+            () -> ClientInit.blueprintButtons.getImageSafe().get().getMask()
+                    .copyWithOp(o -> o.rectangleUV(32f / 128f, 40f / 128f,
+                            48f / 128f, 48f / 128f))
+    );
+
+    private int windowCapacity = 0, windowTop = 0;
+    private ArrayList<TechTreeItemButton> groupButtons;
+    private float bgX = 0, bgY = 0, scale = 1.0f;
 
     public BlueprintScreen(BlueprintMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
         this.showSub = false;
         trees = ClientTechTreeManager.MANAGER.trees();
+        groupButtons = new ArrayList<>();
     }
 
     @Override
-    protected void renderBg(PoseStack poseStack, float partialTick, int mouseX, int mouseY) {
+    protected void init() {
+        super.init();
+        for (ClientTechTree tree : trees.values()) {
+            tree.getGroups().forEach((name, group) -> groupButtons
+                    .add(new TechTreeItemButton(group.getIcon(), 20, 20, (a, b, c) -> {
+
+                    })));
+        }
+    }
+
+    @Override
+    protected void renderBg(@NotNull PoseStack poseStack, float partialTick,
+                            int mouseX, int mouseY) {
         renderBackground(poseStack);
         Minecraft mc = Minecraft.getInstance();
         if (mc.screen == null) return;
@@ -40,17 +74,84 @@ public class BlueprintScreen extends AbstractContainerScreen<BlueprintMenu> {
         int windowHeight = mc.screen.height;
         ImageMask mask = showSub ? bgMask.get() : bgNoSubMask.get();
         int w = (int) (windowWidth * .9f);
-        int h = (int) (mask.getImage().height() * ((float) w / (float) mask.getImage().width()));
-        mask.rectangle(new Vector3f((float) (windowWidth - w) /2, (float) (windowHeight - h) / 2, 0),
+        scale = ((float) w / (float) mask.getImage().width());
+        int h = map(mask.getImage().height(), scale);
+        bgX = (float) (windowWidth - w) / 2;
+        bgY = (float) (windowHeight - h) / 2;
+        mask.rectangle(new Vector3f(bgX, bgY, 0),
                 ImageMask.Axis.X, ImageMask.Axis.Y, true, true, w, h);
         poseStack.pushPose();
         mask.renderToGui(poseStack.last());
         poseStack.popPose();
     }
 
+    private int map(int xOry, float scale) {
+        return (int) (xOry * scale);
+    }
+
+    // leftUp border: (7, 19); (23, 106)
+    private void renderGuidelines(PoseStack poseStack, float partial,
+                                  int mouseX, int mouseY, float scale) {
+        int leftTopX = map(7, scale);
+        int leftTopY = map(19, scale);
+
+        int rightDownX = map(23, scale);
+        int rightDownY = map(106, scale);
+
+        int guideWidth = rightDownX - leftTopX;
+        int guideHeight = rightDownY - leftTopY;
+        int btnHeight = guideHeight - 20;
+        windowCapacity = btnHeight / 20;
+
+        if (windowCapacity <= groupButtons.size()) {
+            ImageMask upArrowMask = upArrow.get();
+            ImageMask downArrowMask = downArrow.get();
+            float btnX = bgX + leftTopX + (float) (guideWidth - 16) / 2;
+
+            if (windowTop > 0) {
+                upArrowMask.rectangle(new Vector3f(btnX, bgY + leftTopY + .5f, 0),
+                        ImageMask.Axis.X, ImageMask.Axis.Y,
+                        true, true, 16, 8);
+                upArrowMask.renderToGui();
+            }
+            if (windowTop + windowCapacity < groupButtons.size()) {
+                downArrowMask.rectangle(new Vector3f(btnX, bgY + rightDownY - 8, 0),
+                        ImageMask.Axis.X, ImageMask.Axis.Y,
+                        true, true, 16, 8);
+                downArrowMask.renderToGui();
+            }
+        }
+        int grpBtnY = Math.round(bgY) + leftTopY + (guideHeight - btnHeight) / 2;
+        int grpBtnX = Math.round(bgX) + leftTopX + (guideWidth - 20) / 2;
+        for (int i = windowTop; i < Math.min(windowTop + windowCapacity, groupButtons.size()); i++) {
+            TechTreeItemButton button = groupButtons.get(i);
+            button.setPosition(grpBtnX, grpBtnY + (i - windowTop) * 20);
+            button.render(poseStack, mouseX, mouseY, partial);
+        }
+    }
+
+    private void testLabel(PoseStack poseStack, float partial, int mouseX, int mouseY) {
+        if (trees.isEmpty()) return;
+        ClientTechTree tree = null;
+        for (ClientTechTree t : trees.values()) {
+            tree = t;
+            break;
+        }
+        ClientTechTreeGroup group = null;
+        for (ClientTechTreeGroup g : tree.getGroups().values()) {
+            group = g;
+            break;
+        }
+        if (group == null) return;
+        ClientTechTreeNode node = group.getRootNode();
+        TechTreeLabel label = TechTreeLabel.smallLabel(node, 0, 0, Component.empty());
+        label.render(poseStack, mouseX, mouseY, partial);
+    }
+
     @Override
-    public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
-        super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
+    public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+        super.render(poseStack, mouseX, mouseY, partialTick);
+        renderGuidelines(poseStack, partialTick, mouseX, mouseY, scale);
     }
 
     @Override
---
+++ src/main/java/willow/train/kuayue/systems/tech_tree/client/gui/TechTreeItemButton.java
@@ -0,0 +1,64 @@
+package willow.train.kuayue.systems.tech_tree.client.gui;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import lombok.Getter;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.AbstractButton;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import willow.train.kuayue.systems.editable_panel.widget.ImageButton;
+import willow.train.kuayue.systems.editable_panel.widget.OnClick;
+
+public class TechTreeItemButton extends AbstractButton {
+
+    private final OnClick<TechTreeItemButton> action;
+
+    @Getter
+    @NotNull private ItemStack stack;
+    public TechTreeItemButton(@NotNull ItemStack item, int pWidth, int pHeight,
+                              OnClick<TechTreeItemButton> action) {
+        super(0, 0, pWidth, pHeight, Component.empty());
+        this.action = action;
+        stack = item;
+    }
+
+    public void setStack(@NotNull ItemStack stack) {
+        this.stack = stack;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setPosition(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    @Override
+    public void onPress() {}
+
+    @Override
+    public void onClick(double pMouseX, double pMouseY) {
+        action.click(this, pMouseX, pMouseY);
+    }
+
+    @Override
+    public void updateNarration(NarrationElementOutput output) {}
+
+    @Override
+    public void renderButton(@NotNull PoseStack poseStack, int mouseX,
+                             int mouseY, float partialTick) {
+        ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
+        int iX = this.x + (this.width - 16) / 2;
+        int iY = this.y + (this.height - 16) / 2;
+        renderer.renderGuiItem(stack, iX, iY);
+    }
+}
