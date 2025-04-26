7a2954c80765e7d88e4dc7a06df2c306966a32be
zlw380 <496607959@qq.com>
2025-04-26 23:09:07 +0800

    DF11地板，设备间车顶A/B。

--- src/main/java/willow/train/kuayue/initial/panel/I11Panel.java
+++ src/main/java/willow/train/kuayue/initial/panel/I11Panel.java
@@ -5,7 +5,10 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import willow.train.kuayue.block.panels.FullShapeDirectionalBlock;
+import willow.train.kuayue.block.panels.slab.HingeSlabBlock;
+import willow.train.kuayue.block.panels.slab.TrainSlabBlock;
 import willow.train.kuayue.initial.AllElements;
+import willow.train.kuayue.initial.registration.SlabRegistration;
 
 public class I11Panel {
 
@@ -29,6 +32,30 @@ public class I11Panel {
                     .addProperty(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .defaultBlockItem()
                     .tabTo(AllElements.neoKuayueLocoTab)
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<TrainSlabBlock> DF11_FLOOR =
+            new SlabRegistration<TrainSlabBlock>("df11_floor")
+                    .block(p -> new TrainSlabBlock(p, false))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_BLACK)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f, 3f)
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<HingeSlabBlock> DF11_CARPORT_EQUIP_B =
+            new SlabRegistration<HingeSlabBlock>("df11_carport_equip_b")
+                    .block(p -> new HingeSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f, 3f)
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<HingeSlabBlock> DF11_CARPORT_EQUIP_A =
+            new SlabRegistration<HingeSlabBlock>("df11_carport_equip_a")
+                    .block(p -> new HingeSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f, 3f)
                     .submit(AllElements.testRegistry);
 
     public static void invoke(){}
