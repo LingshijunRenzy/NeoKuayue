3e1059ddb79bbaf6ceacf695f60b5314a3f6162a
dadads <13969336971@163.com>
2025-04-27 01:21:04 +0800

    SS3

--- src/main/java/willow/train/kuayue/event/client/TooltipEvent.java
+++ src/main/java/willow/train/kuayue/event/client/TooltipEvent.java
@@ -1,11 +1,19 @@
 package willow.train.kuayue.event.client;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import willow.train.kuayue.systems.tech_tree.NodeLocation;
+import willow.train.kuayue.systems.tech_tree.client.ClientTechTreeManager;
+import willow.train.kuayue.systems.tech_tree.client.ClientTechTreeNode;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -16,16 +24,16 @@ public class TooltipEvent {
     public static void onItemTooltip(ItemTooltipEvent event) {
         ItemStack stack = event.getItemStack();
         if (!stack.hasTag()) return;
-        net.minecraft.nbt.CompoundTag tag = stack.getTag();
+        CompoundTag tag = stack.getTag();
         if (!tag.contains("node")) return;
         try {
-            willow.train.kuayue.systems.tech_tree.NodeLocation nodeLoc = new willow.train.kuayue.systems.tech_tree.NodeLocation(tag.getString("node"));
-            willow.train.kuayue.systems.tech_tree.client.ClientTechTreeManager manager = willow.train.kuayue.systems.tech_tree.client.ClientTechTreeManager.getInstance();
-            willow.train.kuayue.systems.tech_tree.client.ClientTechTreeNode node = manager.getNode(nodeLoc);
+            NodeLocation nodeLoc = new NodeLocation(tag.getString("node"));
+            ClientTechTreeManager manager = ClientTechTreeManager.getInstance();
+            ClientTechTreeNode node = manager.getNode(nodeLoc);
             if (node == null) return;
-            net.minecraft.network.chat.MutableComponent component = net.minecraft.network.chat.Component.literal("-> ")
-                    .append(net.minecraft.network.chat.Component.translatable(node.getName()))
-                    .setStyle(net.minecraft.network.chat.Style.EMPTY.withColor(net.minecraft.ChatFormatting.GOLD));
+            MutableComponent component = Component.literal("-> ")
+                    .append(Component.translatable(node.getName()))
+                    .setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD));
             event.getToolTip().add(component);
         } catch (Exception ignored) {}
     }
