c52abb46eb10801b9fac190c5ab416284534de83
aurora4123 <1652039138@qq.com>
2025-04-27 19:02:24 +0800

    DF11 通用上厢板

--- src/main/java/willow/train/kuayue/initial/panel/I11Panel.java
+++ src/main/java/willow/train/kuayue/initial/panel/I11Panel.java
@@ -4,8 +4,11 @@ import kasuga.lib.registrations.common.BlockReg;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.phys.Vec2;
 import willow.train.kuayue.block.panels.FullShapeDirectionalBlock;
+import willow.train.kuayue.block.panels.TrainPanelBlock;
 import willow.train.kuayue.initial.AllElements;
+import willow.train.kuayue.initial.registration.PanelRegistration;
 
 public class I11Panel {
 
@@ -29,6 +32,14 @@ public class I11Panel {
                     .addProperty(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .defaultBlockItem()
                     .tabTo(AllElements.neoKuayueLocoTab)
+                    .submit(AllElements.testRegistry);
+
+    public static final PanelRegistration<TrainPanelBlock> PANEL_TOP_DF11 =
+            new PanelRegistration<TrainPanelBlock>("panel_top_df11")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0,0),new Vec2(1,1)))
+                    .materialAndColor(Material.METAL,MaterialColor.COLOR_BLUE)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f,3f)
                     .submit(AllElements.testRegistry);
 
     public static void invoke(){}
