768b6b6050b65f6cd8c8aac1f9d0494d303ecf47
TheFairyTale <986920241@qq.com>
2025-05-21 23:02:25 +0800

    修复使用水牌物品创建后不可通过刷子编辑的问题

--- src/main/java/willow/train/kuayue/block/panels/TrainPanelBlock.java
+++ src/main/java/willow/train/kuayue/block/panels/TrainPanelBlock.java
@@ -94,10 +94,17 @@ public class TrainPanelBlock extends Block implements IWrenchable, EntityBlock {
         BlockState neoState = pState;
         ItemStack item = pPlayer.getItemInHand(pHand);
         // 手持剪贴板
-        // 手持带颜色刷子
         TrainPanelProperties.EditType orgType = pState.getValue(EDIT_TYPE);
         boolean changed = false;
-        if (item.is(EditablePanelItem.COLORED_BRUSH.getItem())) {
+        // 手持水牌
+        if ((item.is(EditablePanelItem.LAQUERED_BOARD.getItem()) || (item.is(EditablePanelItem.COLORED_BRUSH.getItem()) && TrainPanelProperties.EditType.LAQUERED.equals(orgType))) && pState.is(Objects.requireNonNull(AllTags.BOTTOM_PANEL.tag()))) {
+            neoState = pState.setValue(EDIT_TYPE, TrainPanelProperties.EditType.LAQUERED);
+            changed = orgType != TrainPanelProperties.EditType.LAQUERED;
+            return openScreen(pLevel, pPos, pPlayer, neoState, changed);
+        }
+        // 创建速度文字的唯一途径
+        // 手持带颜色刷子
+        if ((item.is(EditablePanelItem.COLORED_BRUSH.getItem())  || TrainPanelProperties.EditType.SPEED.equals(orgType))) {
             // 带有下厢板标签
             if (pState.is(Objects.requireNonNull(AllTags.BOTTOM_PANEL.tag()))) {
                 neoState = pState.setValue(EDIT_TYPE, TrainPanelProperties.EditType.TYPE);
@@ -109,12 +116,6 @@ public class TrainPanelBlock extends Block implements IWrenchable, EntityBlock {
                 changed = orgType != TrainPanelProperties.EditType.SPEED;
             }
 
-            return openScreen(pLevel, pPos, pPlayer, neoState, changed);
-        }
-        // 手持水牌
-        if (item.is(EditablePanelItem.LAQUERED_BOARD.getItem()) && pState.is(Objects.requireNonNull(AllTags.BOTTOM_PANEL.tag()))) {
-             neoState = pState.setValue(EDIT_TYPE, TrainPanelProperties.EditType.LAQUERED);
-             changed = orgType != TrainPanelProperties.EditType.LAQUERED;
             return openScreen(pLevel, pPos, pPlayer, neoState, changed);
         }
         // 手持贴纸
