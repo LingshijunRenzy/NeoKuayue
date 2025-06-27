b8d48fb3f6aaac997dd3515582acaecf49162184
zlw380 <496607959@qq.com>
2025-06-27 14:28:36 +0800

    金鹰290排障器，地板，车头I端。

--- src/main/java/willow/train/kuayue/initial/panel/IJY290Panel.java
+++ src/main/java/willow/train/kuayue/initial/panel/IJY290Panel.java
@@ -5,7 +5,9 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import willow.train.kuayue.block.panels.FullShapeDirectionalBlock;
+import willow.train.kuayue.block.panels.slab.TrainSlabBlock;
 import willow.train.kuayue.initial.AllElements;
+import willow.train.kuayue.initial.registration.SlabRegistration;
 
 public class IJY290Panel {
 
@@ -18,6 +20,44 @@ public class IJY290Panel {
                     .addProperty(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .defaultBlockItem()
                     .tabTo(AllElements.neoKuayueLocoTab)
+                    .submit(AllElements.testRegistry);
+
+    public static final BlockReg<FullShapeDirectionalBlock> JY290_HEAD_1 =
+            new BlockReg<FullShapeDirectionalBlock>("jy290_head_1")
+                    .blockType(FullShapeDirectionalBlock::new)
+                    .material(Material.METAL).materialColor(MaterialColor.COLOR_BLACK)
+                    .addProperty(BlockBehaviour.Properties::noOcclusion)
+                    .addProperty(properties -> properties.strength(1.5f, 3f))
+                    .addProperty(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+                    .defaultBlockItem()
+                    .tabTo(AllElements.neoKuayueLocoTab)
+                    .submit(AllElements.testRegistry);
+
+    public static final BlockReg<FullShapeDirectionalBlock> JY290_COWCATCHER =
+            new BlockReg<FullShapeDirectionalBlock>("jy290_cowcatcher")
+                    .blockType(FullShapeDirectionalBlock::new)
+                    .material(Material.METAL).materialColor(MaterialColor.COLOR_BLACK)
+                    .addProperty(properties -> properties.strength(1.5f, 3f))
+                    .addProperty(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+                    .defaultBlockItem()
+                    .addProperty(BlockBehaviour.Properties::noOcclusion)
+                    .tabTo(AllElements.neoKuayueLocoTab)
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<TrainSlabBlock> JY290_FLOOR =
+            new SlabRegistration<TrainSlabBlock>("jy290_floor")
+                    .block(p -> new TrainSlabBlock(p, false))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_BLACK)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f, 3f)
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<TrainSlabBlock> JY290_FLOOR_LADDER =
+            new SlabRegistration<TrainSlabBlock>("jy290_floor_ladder")
+                    .block(p -> new TrainSlabBlock(p, false))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_BLACK)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f, 3f)
                     .submit(AllElements.testRegistry);
 
     public static void invoke(){}
